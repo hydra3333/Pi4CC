f2c1815f01af2ace10bf3f81f0055f848be12000
hydra3333 <hydra3333@gmail.com>
2019-12-13 06:42:19 +1030

    manual reload script

---
+++ reload_media.js.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# to get rid of MSDOS format do this to this file: sudo sed -i s/\\r//g ./filename
+# or, open in nano, control-o and then then alt-M a few times to toggle msdos format off and then save
+
+set -x
+
+python3 /var/www/Pi4CC/2019.12.10-create-json.py --source_folder /mnt/mp4library/mp4library ---filename-extension mp4 --json_file /var/www/Pi4CC/media.js > /var/www/Pi4CC/create-json.log 2>&1
+
+cat /var/www/Pi4CC/create-json.log
