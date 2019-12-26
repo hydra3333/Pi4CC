e1035d040840573c37a574df0a6653b9296af32f
hydra3333 <hydra3333@gmail.com>
2019-12-26 20:59:57 +1030

    use pymediainfo to retrieve .mp4 characteristics

--- 2019.12.10-create-json.py
+++ 2019.12.10-create-json.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 #
+# sudo apt install mediainfo
+# pip3 install pymediainfo
+#
 # Create a JSON file of media in a folder tree, PRE-SORTED IN FOLDER ORDER (the code which uses this depends on that).
 # The JSON file produced can be consumed by variants of the Google Chromecast example CastVideos-chrome (2019.12.10)
 # which can be found at https://github.com/googlecast/CastVideos-chrome
@@ -12,6 +15,7 @@ import fnmatch
 import argparse
 from collections import defaultdict
 from requests.utils import requote_uri
+from pymediainfo import MediaInfo
 #
 def find_matching_files(foldername, file_pattern):
     # foldername could be a relative path, so transform it into an absolute path
@@ -74,20 +78,49 @@ if __name__ == '__main__':
         jf.write(f"      // ----- START of folder({record_number}) {part_url} ... files={len(the_filenames)} \n")
         for c,the_filename in enumerate(sorted(the_filenames,key=lambda i: i[0].casefold())):
         #for c,the_filename in enumerate(sorted(the_filenames,key=lambda i: i[0].lower())):
+            the_media_file = the_folder + "/" + the_filename
+            #print(f"        file='{the_media_file}'")
+            media_info = MediaInfo.parse(the_media_file)
+            video_resolution = ""
+            video_duration = ""
+            video_codec = ""
+            audio_codec = ""
+            for track in media_info.tracks:  # #for k in track.to_data().keys():
+                #    print("{}.{}={}".format(track.track_type,k,track.to_data()[k]))
+                if track.track_type == 'Video':
+                    #print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                    #print("{} width                 {}".format(track.track_type,track.to_data()["width"]))
+                    #print("{} height                {}".format(track.track_type,track.to_data()["height"]))
+                    #print("{} duration              {}s".format(track.track_type,track.to_data()["duration"]/1000.0))
+                    #print("{} duration              {}".format(track.track_type,track.to_data()["other_duration"][3][0:8]))
+                    #print("{} other_format          {}".format(track.track_type,track.to_data()["other_format"][0]))
+                    #print("{} codec_id              {}".format(track.track_type,track.to_data()["codec_id"]))
+                    #print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                    video_duration = track.to_data()["other_duration"][3][0:8]
+                    video_resolution = "{}x{}".format(track.to_data()["width"],track.to_data()["height"])
+                    video_codec = track.to_data()["other_format"][0]
+                elif track.track_type == 'Audio':
+                    #print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                    #print("{} format                {}".format(track.track_type,track.to_data()["format"]))
+                    #print("{} codec_id              {}".format(track.track_type,track.to_data()["codec_id"]))
+                    #print("{} channel_s             {}".format(track.track_type,track.to_data()["channel_s"]))
+                    #print("{} other_channel_s       {}".format(track.track_type,track.to_data()["other_channel_s"][0]))
+                    #print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+                    audio_codec = track.to_data()["format"]
             title,ignore_me = os.path.splitext(the_filename.replace("'","-"))  # root,ext = os.path.splitext(path) 
             source = requote_uri("http://10.0.0.6/mp4library" + part_url + the_filename)
             subtitle = source
             thumb = "".replace("'","-")
-            duration = 600
-            resolution = "1920×1080".replace("'","-")
             jf.write("      {\n")
-            jf.write(f"        'title'      : '{title}',\n")
-            jf.write(f"        'subtitle'   : '{subtitle}',\n")
-            jf.write(f"        'sources'    : ['{source}',],\n")
-            jf.write(f"        'thumb'      : '',\n")
-            jf.write(f"        'duration'   : {duration},\n")
-            jf.write(f"        'resolution' : '{resolution}',\n")
-            jf.write(f"        'folder'     : '{txt_part_url}',\n")
+            jf.write(f"        'title'       : '{title}',\n")
+            jf.write(f"        'subtitle'    : '{subtitle}',\n")
+            jf.write(f"        'sources'     : ['{source}',],\n")
+            jf.write(f"        'thumb'       : '',\n")
+            jf.write(f"        'duration'    : '{video_duration}',\n")
+            jf.write(f"        'resolution'  : '{video_resolution}',\n")
+            jf.write(f"        'video_codec' : '{video_codec}',\n")
+            jf.write(f"        'audio_codec' : '{audio_codec}',\n")
+            jf.write(f"        'folder'      : '{txt_part_url}',\n")
             jf.write("      },\n")
             cc=cc+1
         jf.write(f"      // ----- END   of folder({record_number}) {part_url} ... files={len(the_filenames)} \n")
