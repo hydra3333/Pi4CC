635ad959f20d3399bf7b746416028f63f561e5c8
hydra3333 <hydra3333@gmail.com>
2019-12-31 19:27:42 +1030

    Update reload_media.js.sh

--- reload_media.js.sh
+++ reload_media.js.sh
@@ -4,7 +4,7 @@
 
 set -x
 
-python3 /var/www/Pi4CC/create-json.py --source_folder /mnt/mp4library/mp4library --filename-extension mp4 --json_file /var/www/Pi4CC/media.js > /var/www/Pi4CC/create-json.log 2>&1
+python3 /var/www/Pi4CC/create-json.py --source_folder /mnt/mp4library/mp4library --filename-extension mp4 --json_file /var/www/Pi4CC/media.js 2>&1 > /var/www/Pi4CC/create-json.log
 #
 cat /var/www/Pi4CC/create-json.log
 #
