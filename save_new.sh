43d162f3e106fb29655e590b3d89bf63d0b90f42
hydra3333 <hydra3333@gmail.com>
2020-01-03 19:23:04 +1030

    Add files via upload

---
+++ save_new.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# to get rid of MSDOS format do this to this file: sudo sed -i s/\\r//g ./filename
+# or, open in nano, control-o and then then alt-M a few times to toggle msdos format off and then save
+
+set -x
+
+sudo chown -R pi:www-data /var/www-old
+sudo chown -R pi:www-data /var/www-new
+sudo chown -R pi:www-data /var/www
+sudo chmod a=rwx -R /var/www-old
+sudo chmod a=rwx -R /var/www-new
+sudo chmod a=rwx -R /var/www
+
+cp -fvR /var/www/* /var/www-new/
+
+sudo chown -R pi:www-data /var/www-old
+sudo chown -R pi:www-data /var/www-new
+sudo chown -R pi:www-data /var/www
+sudo chmod a=rwx -R /var/www-old
+sudo chmod a=rwx -R /var/www-new
+sudo chmod a=rwx -R /var/www
+
+#sleep 3s
+#systemctl restart apache2
+sudo service apache2 restart
+sleep 10s
+
+exit
