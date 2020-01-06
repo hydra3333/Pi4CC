3a391afc25619539aaf5533b618e9f0789a49ef6
hydra3333 <hydra3333@gmail.com>
2020-01-06 14:07:01 +1030

    Add files via upload

--- setup_1.sh
+++ setup_1.sh
@@ -56,7 +56,80 @@ else  # config file does not exist, prompt normally with successive defaults bas
     read -e -p "Designate the root folder on the USB3 external hard drive [${server_root_folder_default}]: " -i "${server_root_folder_default}" input_string
     server_root_folder="${input_string:-$server_root_folder_default}" # forces the name to be the original default if the user erases the input or default (submitting a null).
 fi
-echo "(re)saving the new answers to the config file for re-use as future defaults..."
+# ALWAYS choose a USB3 Disk device and find it's UUID
+# (The use/positioning of parentheses and curly-brackets in setting array elements is critical)
+disk_name=()
+device_label=()
+device_uuid=()
+device_fstype=()
+device_size=()
+device_mountpoint=()
+while IFS= read -r -d $'\0' device; do
+   d=$device
+   device=${d/\/dev\//}
+   x_disk_name=($device)
+   x_device_name=($d)
+   x_device_label="$(lsblk -n -p -l -o label ${d})"
+   #x_device_uuid="$(blkid -o value -s UUID ${d})"
+   x_device_uuid="$(lsblk -n -p -l -o uuid ${d})"
+   x_device_fstype="$(lsblk -n -p -l -o fstype ${d})"
+   x_device_size="$(lsblk -n -p -l -o size ${d})"
+   x_device_mountpoint="$(lsblk -n -p -l -o mountpoint ${d})"
+   if [[ "${x_device_uuid}" != "" ]] ; then
+      #echo  "device valid ${x_device_name}"
+      disk_name+="${x_disk_name}"
+      device_name+="${x_device_name}"
+      device_label+="${x_device_label}"
+      #device_uuid+="${x_devfice_uuid}"
+      device_uuid+="${x_device_uuid}"
+      device_fstype+="${x_device_fstype}"
+      device_size+="${x_device_size}"
+      device_mountpoint+="${x_device_mountpoint}"
+   fi
+done < <(find "/dev/" -regex '/dev/sd[a-z][0-9]\|/dev/vd[a-z][0-9]\|/dev/hd[a-z][0-9]' -print0)
+device_string_tabbed=()
+device_string=()
+for i in `seq 0 $((${#disk_name[@]}-1))`; do
+   device_string+=("DISK=${disk_name[$i]}, DEVICE==${device_name[$i]}, LABEL=${device_label}, UUID=${device_uuid[$i]}, FS_TYPE=${device_fstype[$i]}, SIZE=${device_size[$i]}, MOUNT_POINT=${device_mountpoint[$i]}")
+   device_string_tabbed+=("${disk_name[$i]}\t${name[$i]}\t${size[$i]}\t${device_name[$i]}\t${device_label}\t${device_uuid[$i]}\t${device_fstype[$i]}\t${device_size[$i]}\t${device_mountpoint[$i]}")
+done
+#for i in `seq 0 $((${#disk_name[@]}-1))`; do
+#   echo -e "${i} ${device_string_tabbed[$i]}"
+#done
+#for i in `seq 0 $((${#disk_name[@]}-1))`; do
+#   echo -e "${i} ${device_string[$i]}"
+#done
+#---
+#---
+menu_from_array () {
+ select item; do
+   # Check the selected menu item number
+   if [ 1 -le "$REPLY" ] && [ "$REPLY" -le $# ]; then
+      #echo "The selected operating system is $REPLY $item"
+      if [[ "$item" = "$2" ]] ; then
+         echo "$2 ..."
+         exit
+      fi
+      let "selected_index=${REPLY} - 1"
+      selected_item=${item}
+      #echo "The selected operating system is ${selected_index} ${selected_item}"
+      break;
+   else
+      echo "Invalid selection: Select any number from 1-$#"
+   fi
+ done
+}
+echo ""
+echo "Choose which device is the USB3 hard drive/partition containing the .mp4 files ! "
+echo ""
+exit_string="It isn't displayed, Exit immediately"
+menu_from_array "${device_string[@]}" "${exit_string}"
+server_USB3_DISK_NAME="${disk_name[${selected_index}]}"
+server_USB3_DEVICE_NAME="${device_name[${selected_index}]}"
+server_USB3_DEVICE_UUID="${device_uuid[${selected_index}]}"
+echo ""
+#
+echo "(re)Saving the new answers to the config file for re-use as future defaults..."
 sudo rm -fv "$setup_config_file"
 echo "#" >> "$setup_config_file"
 echo "server_name_default=${server_name}">> "$setup_config_file"
@@ -64,6 +137,9 @@ echo "server_ip_default=${server_ip}">> "$setup_config_file"
 echo "server_alias_default=${server_alias}">> "$setup_config_file"
 echo "server_root_USBmountpoint_default=${server_root_USBmountpoint}">> "$setup_config_file"
 echo "server_root_folder_default=${server_root_folder}">> "$setup_config_file"
+echo "server_USB3_DISK_NAME=${server_USB3_DISK_NAME}">> "$setup_config_file"
+echo "server_USB3_DEVICE_NAME=${server_USB3_DEVICE_NAME}">> "$setup_config_file"
+echo "server_USB3_DEVICE_UUID=${server_USB3_DEVICE_UUID}">> "$setup_config_file"
 echo "#">> "$setup_config_file"
 set -x
 sudo chmod -c a=rwx -R "$setup_config_file"
@@ -75,6 +151,9 @@ echo "                server_ip=${server_ip}"
 echo "             server_alias=${server_alias}"
 echo "server_root_USBmountpoint=${server_root_USBmountpoint}"
 echo "       server_root_folder=${server_root_folder}"
+echo "    server_USB3_DISK_NAME=${server_USB3_DISK_NAME}"
+echo "  server_USB3_DEVICE_NAME=${server_USB3_DEVICE_NAME}"
+echo "  server_USB3_DEVICE_UUID=${server_USB3_DEVICE_UUID}"
 echo ""
 echo "# ------------------------------------------------------------------------------------------------------------------------"
 
