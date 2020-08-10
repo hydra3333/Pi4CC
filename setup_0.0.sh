068379818ef21df0ba676ee8448b694a21e311fc
hydra3333 <hydra3333@gmail.com>
2020-08-10 19:40:33 +0930

    up boot delay from 15 secs to 30 secs

--- setup_0.0.sh
+++ setup_0.0.sh
@@ -63,17 +63,17 @@ echo ""
 read -p "Hope the Reserved IP worked, please check the IP and hostname.  Press Enter to continue"
 
 echo ""
-echo "# Add 15 seconds for the USB3 drive to spin up.  Add the line at the top."
+echo "# Add 30 seconds for the USB3 drive to spin up.  Add the line at the top."
 echo "# per https://www.raspberrypi.org/documentation/configuration/config-txt/boot.md"
 echo ""
-echo "# Adding a line 'boot_delay=15' at the top of '/boot/config.txt'"
+echo "# Adding a line 'boot_delay=30' at the top of '/boot/config.txt'"
 echo ""
-#sudo sed -i "1 i boot_delay=15" "/boot/config.txt" # doesn't work if the file has no line 1
+#sudo sed -i "1 i boot_delay=30" "/boot/config.txt" # doesn't work if the file has no line 1
 set -x
 sudo cp -fv "/boot/config.txt" "/boot/config.txt.old"
 rm -f ./tmp.tmp
 sudo sed -i "/boot_delay/d" "/boot/config.txt"
-echo "boot_delay=15" > ./tmp.tmp
+echo "boot_delay=30" > ./tmp.tmp
 sudo cat /boot/config.txt >> ./tmp.tmp
 sudo cp -fv ./tmp.tmp /boot/config.txt
 rm -f ./tmp.tmp
