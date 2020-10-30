1c9db7a07ff4c1ae3354fd902ebb34502ea478d9
hydra3333 <hydra3333@gmail.com>
2020-10-31 09:27:56 +1030

    Add files via upload

--- setup_0.0.sh
+++ setup_0.0.sh
@@ -77,7 +77,7 @@ echo "boot_delay=30" > ./tmp.tmp
 sudo cat /boot/config.txt >> ./tmp.tmp
 sudo cp -fv ./tmp.tmp /boot/config.txt
 rm -f ./tmp.tmp
-diff -U 3 "/boot/config.txt.old" "/boot/config.txt"
+diff -U 10 "/boot/config.txt.old" "/boot/config.txt"
 cat "/boot/config.txt"
 set +x
 
@@ -222,7 +222,7 @@ echo " You MUST check /etc/fstab NOW ... if it is incorrect then abort this proc
 echo " You MUST check /etc/fstab NOW ... if it is incorrect then abort this process NOW and fix it manually"
 echo ""
 set -x
-diff -U 3 "/etc/fstab.old" "/etc/fstab" 
+diff -U 10 "/etc/fstab.old" "/etc/fstab" 
 set +x
 echo ""
 set -x
